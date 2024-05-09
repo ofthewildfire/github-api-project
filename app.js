e7fd05c32dc4a684059a25a613b2b78725bc8b87
ofthewildfire <103148299+ofthewildfire@users.noreply.github.com>
2024-05-09 21:46:09 +0200

    Completed Project: Working on CSS

--- app.js
+++ app.js
@@ -1,17 +1,52 @@
 const userSearchInput = document.getElementById("user-search");
 const searchButton = document.getElementById("search-button");
 
-searchButton.addEventListener("click", (e) => {
+// user content elements
+
+const userAvatar = document.getElementById("user-avatar");
+const userName = document.getElementById("user-name");
+const userLogin = document.getElementById("user-login");
+const userBio = document.getElementById("user-bio");
+const userLocation = document.getElementById("user-location");
+const userRepos = document.getElementById("user-repos");
+const userFollowers = document.getElementById("user-followers");
+const userFollowing = document.getElementById("user-following");
+const userCreatedAt = document.getElementById("user-createdAt");
+const userTwitter = document.getElementById("user-twitter");
+const userBlog = document.getElementById("user-blog");
+const userCompany = document.getElementById("user-company");
+
+// Search button event listener
+searchButton.addEventListener("click", async (e) => {
   e.preventDefault();
-  let resultFromAPI = getUserFromAPI(userSearchInput.value);
-  console.log(resultFromAPI);
+  let data = await populateHTML(getUserFromAPI(userSearchInput.value));
+  userLogin.textContent = data.login;
+  userName.textContent = data.name;
+  userAvatar.src = data.avatar_url;
+  userCreatedAt.textContent = `Joined ${data.created_at}`;
+  userBio.textContent = data.bio;
+  userRepos.textContent = data.public_repos;
+  userFollowers.textContent = data.followers;
+  userFollowing.textContent = data.following;
+  userLocation.textContent = data.location ? data.location : "Not Available";
+  userTwitter.textContent = data.twitter_username
+    ? data.twitter_username
+    : "Not Available";
+  userTwitter.href = data.twitter_username
+    ? `https://twitter.com/${data.twitter_username}`
+    : "#";
+  userBlog.href = data.blog ? data.blog : "#";
+  userBlog.textContent = data.blog ? data.blog : "Not Available";
+  userCompany.textContent = data.company ? data.company : "Not Available";
+  console.log(data);
 });
 
-function getUserFromAPI(userName) {
-  return fetch(`https://api.github.com/users/${userName}`)
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-      return data;
-    });
+async function getUserFromAPI(userName) {
+  const response = await fetch(`https://api.github.com/users/${userName}`);
+  const data = await response.json();
+  return data;
+}
+
+function populateHTML(data) {
+  return data;
 }
